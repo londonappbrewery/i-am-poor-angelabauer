6cf3996230c9b63ee62da2b00e71f79e72a7fd9e
frankding123456 <48672825+frankding123456@users.noreply.github.com>
2019-04-02 11:34:18 +1100

    sfdsafdsaf

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated. dfasfdasfasfsafs
     }
 
 
